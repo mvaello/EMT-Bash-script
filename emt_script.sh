9766a645c7da8c514f24ea7e8e693df7b106327b
Miguel Vaello <miguel.vaellomartinez@gmail.com>
2012-07-24 02:31:47 +0200

    Fixed small error messages

--- emt_script.sh
+++ emt_script.sh
@@ -5,13 +5,14 @@ USO="Uso: $0 [linea] [cod. parada]"
 # Si no se pasa ningún parámetro, se muestra el uso del comando
 # Si el número de parámetro es cero (zero -z)
 if [ -z $# ]; then 
+    echo "Error: No se ha introducido ningún parámetro"
 	echo $USO
 	exit 1
 fi
 # Si el número de parámetros introducidos es superior a 2, se muestra el error
 # diciendo que hay demasiados parámetros, se muestra el uso y se sale (exit)
 if [ $# -gt 2 ]; then
-	echo "Demasiados parámetros"
+	echo "Error: Demasiados parámetros"
 	echo $USO
 	exit 1
 fi
@@ -34,6 +35,6 @@ fi
 # la línea y la parada de bus así como la plataforma que recibe los datos, en este caso Java. 
 salida=$(curl -s http://195.76.144.242:80/services/tespera2.asp -d linea=$LINEA -d parada=$PARADA -d movil=JAVA)
 
-# Para hacerlo compatible con el comando say de Mac, se ha modificado el stream original
+# Para hacerlo compatible con el comando 'say' de OS X, para ello se ha modificado el stream original
 salida=$(echo $salida | sed 's .\{1\}  ')
 echo $salida
